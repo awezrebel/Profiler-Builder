5f1eaaaf8642be173579e2dd0e3f7bf47139d5f1
Shaik Awez <59769777+awezrebel@users.noreply.github.com>
2021-04-06 20:12:55 +0530

    Update profileserver.js

--- profileserver.js
+++ profileserver.js
@@ -85,7 +85,7 @@ c+=1;
 var connection = mysql.createConnection({
 host: "database-1.cxg5ddbyrmb4.us-east-1.rds.amazonaws.com",
 user: "admin",
-password: "12345678",
+password: "SE@Profile-Builder#",
 port: "3306",
 database: "database1"
 });
